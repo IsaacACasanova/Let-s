a1bdafb8e1d1cbb4c61d92c1a6c1d932c5488ed6
IsaacACasanova <Isaac.Casanova@uconn.edu>
2014-04-26 17:53:48 -0400

    Commit AttendingCell.m.

--- Let's Test/AttendingCell.m
+++ Let's Test/AttendingCell.m
@@ -22,18 +22,19 @@
 - (void)awakeFromNib
 {
     UIColor* mainColor = [UIColor colorWithRed:68.0/255 green:106.0/255 blue:201.0/255 alpha:1.0f];
-    //UIColor* mainColor = [UIColor colorWithRed:0.0/255 green:87.0/255 blue:255.0/255 alpha:1.0f];
-    //UIColor* neutralColor = [UIColor colorWithWhite:0.4 alpha:1.0];
-    
-    //UIColor* mainColorLight = [UIColor colorWithRed:28.0/255 green:158.0/255 blue:121.0/255 alpha:0.4f];
-    //UIColor* lightColor = [UIColor colorWithWhite:0.7 alpha:1.0];
-    
-    //NSString* fontName = @"Avenir-Book";
-    //NSString* boldItalicFontName = @"Avenir-BlackOblique";
+    UIColor* mainColorLight = [UIColor colorWithRed:68.0/255 green:106.0/255 blue:201.0/255 alpha:0.4f];
+
+
     NSString* boldFontName = @"Avenir-Black";
     
     self.NameLabel.textColor =  mainColor;
     self.NameLabel.font =  [UIFont fontWithName:boldFontName size:14.0f];
+    
+    self.ProfileImage.clipsToBounds = YES;
+    self.ProfileImage.layer.cornerRadius = 25.0f;
+    self.ProfileImage.layer.borderWidth = 2.0f;
+    self.ProfileImage.layer.borderColor = mainColorLight.CGColor;
+    
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
