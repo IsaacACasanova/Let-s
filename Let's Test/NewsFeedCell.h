59a3aa58393d17d95cc0bdb55ccc3cd5c3574e16
IsaacACasanova <Isaac.Casanova@uconn.edu>
2014-04-26 17:52:15 -0400

    Committing NewsfeedCell.h.

--- Let's Test/NewsFeedCell.h
+++ Let's Test/NewsFeedCell.h
@@ -13,7 +13,7 @@
 @property (strong, nonatomic) IBOutlet UILabel *EventLabel;
 @property (strong, nonatomic) IBOutlet UILabel *DecriptionLabel;
 @property (strong, nonatomic) IBOutlet UIImageView *ProfileImage;
-@property (strong, nonatomic) IBOutlet UILabel *commentLabel;
+//@property (strong, nonatomic) IBOutlet UILabel *commentLabel;
 @property (strong, nonatomic) IBOutlet UILabel *dateLabel;
 @property (strong, nonatomic) IBOutlet UILabel *timeStamp;
 
