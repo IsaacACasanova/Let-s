858eab45c075bc2c23fe0eb657928279f2e2b7d6
Jack Gallagher <jackgallaghersp4@gmail.com>
2014-04-30 17:19:01 -0400

    Jibbied.

---
+++ FindAFriend.h
@@ -0,0 +1,15 @@
+//
+//  FindAFriend.h
+//  Let's Test
+//
+//  Created by Jack Gallagher on 4/30/14.
+//  Copyright (c) 2014 Let's. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FindAFriend : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *SearchTextBar;
+@property (weak, nonatomic) IBOutlet UIButton *SearchButton;
+
+@end
